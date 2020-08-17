f6b4bee6be6a97ec66a3ee75efa0450b9c4455a3
gajanan15 <gajanandesai97@gmail.com>
2020-08-17 15:49:08 +0530

    [Gajanan]ADDED : Test And Code For Calculate Total Fare

--- CabInvoiceGenerator/CabInvoiceGenerator.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator.cs
@@ -11,5 +11,20 @@ namespace CabInvoiceGenerator
     /// </summary>
     public class CabInvoiceGenerator
     {
+        private static readonly double MINIMUMCOSTPERKILOMETER = 10.0;
+        private static readonly int COSTPERTIME = 1;
+        private static readonly double MINIMUMFARE = 5.0;
+
+        /// <summary>
+        /// Create Calculate Fare Method.
+        /// </summary>
+        /// <param name="distance"></param>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public double CalculateFare(double distance, int time)
+        {
+            double premiumTotalFare = (distance * MINIMUMCOSTPERKILOMETER) + (time * COSTPERTIME);
+            return Math.Max(premiumTotalFare, MINIMUMFARE);
+        }
     }
 }
--- CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
+++ CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
@@ -5,27 +5,46 @@
 namespace CabInvoiceGeneratorTest
 {
     using NUnit.Framework;
+    using CabInvoiceGenerator;
 
     /// <summary>
     /// Create Test Class.
     /// </summary>
     public class CabInvoiceGeneratorTest
     {
+        private CabInvoiceGenerator cabInvoiceGenerator;
+
         /// <summary>
         /// Craete SetUp Method.
         /// </summary>
         [SetUp]
         public void Setup()
         {
+            this.cabInvoiceGenerator = new CabInvoiceGenerator();
         }
 
         /// <summary>
-        /// Craete Test Case.
+        /// Craete Test For Calculate Total Fare.
         /// </summary>
         [Test]
-        public void Test1()
+        public void GivenDistanceAndTime_ShouldReturnTotalFare()
         {
-            Assert.Pass();
+            double distance = 2.0;
+            int time = 5;
+            double fare = this.cabInvoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(25, fare);
+        }
+
+        /// <summary>
+        /// Craete Test For Calculate Minimum Fare.
+        /// </summary>
+        [Test]
+        public void GivenLessDistanceAndTime_ShouldReturnMinimumFare()
+        {
+            double distance = 0.1;
+            int time = 1;
+            double fare = this.cabInvoiceGenerator.CalculateFare(distance, time);
+            Assert.AreEqual(5.0d, fare);
         }
     }
 }
