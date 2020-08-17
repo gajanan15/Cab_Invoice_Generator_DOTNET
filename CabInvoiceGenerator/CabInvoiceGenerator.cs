04081b6a3868c8508284bc81b25b19cecc65194a
gajanan15 <gajanandesai97@gmail.com>
2020-08-17 15:19:54 +0530

    [Gajanan]ADDED : Welcome To Cab Invoice Generator Problem

---
+++ CabInvoiceGenerator/CabInvoiceGenerator.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="CabInvoiceGenerator.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CabInvoiceGenerator
+{
+    using System;
+
+    /// <summary>
+    /// Create Cab Invoice Generator Class.
+    /// </summary>
+    public class CabInvoiceGenerator
+    {
+    }
+}
---
+++ CabInvoiceGeneratorTest/CabInvoiceGeneratorTest.cs
@@ -0,0 +1,31 @@
+// <copyright file="CabInvoiceGeneratorTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace CabInvoiceGeneratorTest
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Create Test Class.
+    /// </summary>
+    public class CabInvoiceGeneratorTest
+    {
+        /// <summary>
+        /// Craete SetUp Method.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// <summary>
+        /// Craete Test Case.
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
